2fa21755b44c1228014686371f9383b08b492c3e
Lorenzo Maturano <maturanolorenzo@gmail.com>
2024-10-10 12:18:44 -0300

    feat: splitting into modules

---
+++ src/field_element.rs
@@ -0,0 +1,191 @@
+use crate::Error;
+
+#[derive(Debug, Clone)]
+pub struct FieldElement {
+    num: u64,
+    prime: u64,
+}
+
+#[allow(unused)]
+impl FieldElement {
+    pub fn new(num: u64, prime: u64) -> Result<FieldElement, Error> {
+        if num >= prime {
+            return Err(Error::ValueError);
+        }
+
+        Ok(FieldElement { num, prime })
+    }
+
+    fn add(self, rhs: FieldElement) -> Result<Self, Error> {
+        if self.prime != rhs.prime {
+            return Err(Error::TypeError(
+                "Cannot add two numbers in different fields".to_string(),
+            ));
+        }
+
+        let num = (self.num + rhs.num).rem_euclid(self.prime);
+
+        FieldElement::new(num, self.prime)
+    }
+
+    fn sub(self, rhs: FieldElement) -> Result<Self, Error> {
+        if self.prime != rhs.prime {
+            return Err(Error::TypeError(
+                "Cannot subtract numbers in different fields".to_string(),
+            ));
+        }
+
+        let a_num = i64::try_from(self.num).map_err(|e| Error::Conversion(e))?;
+        let b_num = i64::try_from(rhs.num).map_err(|e| Error::Conversion(e))?;
+        let c_num = i64::try_from(self.prime).map_err(|e| Error::Conversion(e))?;
+
+        let num = (a_num as i64 - b_num as i64).rem_euclid(c_num);
+        let num = u64::try_from(num).map_err(|e| Error::Conversion(e))?;
+
+        FieldElement::new(num, self.prime)
+    }
+
+    fn mul(self, rhs: FieldElement) -> Result<Self, Error> {
+        let mut result = FieldElement::new(0, self.prime)?;
+        let mut count = rhs.num;
+
+        while count > 0 {
+            result = result.add(self.clone())?;
+            count -= 1;
+        }
+
+        Ok(result)
+    }
+
+    fn pow(self, exponent: i64) -> Result<Self, Error> {
+        let prime_minus_one = i64::try_from(self.prime - 1).map_err(|e| Error::Conversion(e))?;
+        let exponent = u32::try_from(exponent.rem_euclid(prime_minus_one))
+            .map_err(|e| Error::Conversion(e))?;
+        let result = (self.num.pow(exponent)).rem_euclid(self.prime);
+
+        Ok(FieldElement::new(result, self.prime)?)
+    }
+
+    fn div(self, rhs: FieldElement) -> Result<Self, Error> {
+        let b = rhs.pow(-1)?;
+        let res = self.mul(b)?;
+
+        Ok(res)
+    }
+}
+
+impl std::fmt::Display for FieldElement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "FieldElement_{}({})", self.num, self.prime)
+    }
+}
+
+impl std::cmp::PartialEq for FieldElement {
+    fn eq(&self, other: &Self) -> bool {
+        self.num == other.num && self.prime == other.prime
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn eq() {
+        let a = FieldElement::new(7, 13).unwrap();
+        let b = FieldElement::new(6, 13).unwrap();
+
+        assert!(a != b);
+        assert!(a == a);
+    }
+
+    #[test]
+    fn add() {
+        let p = 57;
+
+        let a = FieldElement::new(44, p).unwrap();
+        let b = FieldElement::new(33, p).unwrap();
+
+        assert_eq!(a.add(b).unwrap(), FieldElement::new(20, p).unwrap());
+
+        let a = FieldElement::new(9, p).unwrap();
+        let b = FieldElement::new(29, p).unwrap();
+
+        assert_eq!(a.sub(b).unwrap(), FieldElement::new(37, p).unwrap());
+
+        let a = FieldElement::new(17, p).unwrap();
+        let b = FieldElement::new(42, p).unwrap();
+        let c = FieldElement::new(49, p).unwrap();
+
+        let actual = a.add(b).unwrap().add(c).unwrap();
+        assert_eq!(actual, FieldElement::new(51, p).unwrap());
+    }
+
+    #[test]
+    fn sub() {
+        let p = 57;
+
+        let a = FieldElement::new(52, p).unwrap();
+        let b = FieldElement::new(30, p).unwrap();
+        let c = FieldElement::new(38, p).unwrap();
+
+        let actual = a.sub(b).unwrap().sub(c).unwrap();
+
+        assert_eq!(actual, FieldElement::new(41, p).unwrap());
+    }
+
+    #[test]
+    fn mul() {
+        let p = 97;
+        let a = FieldElement::new(95, p).unwrap();
+        let b = FieldElement::new(45, p).unwrap();
+        let c = FieldElement::new(31, p).unwrap();
+
+        let actual = a.mul(b).unwrap().mul(c).unwrap();
+
+        assert_eq!(actual, FieldElement::new(23, p).unwrap());
+
+        let a = FieldElement::new(17, p).unwrap();
+        let b = FieldElement::new(13, p).unwrap();
+        let c = FieldElement::new(19, p).unwrap();
+        let d = FieldElement::new(44, p).unwrap();
+
+        let actual = a.mul(b).unwrap().mul(c).unwrap().mul(d).unwrap();
+
+        assert_eq!(actual, FieldElement::new(68, p).unwrap());
+    }
+
+    #[test]
+    fn pow() {
+        let p = 19;
+        let a = FieldElement::new(7, p).unwrap();
+
+        let actual = a.pow(3).unwrap();
+
+        assert_eq!(actual, FieldElement::new(1, p).unwrap());
+
+        let a = FieldElement::new(9, p).unwrap();
+
+        let actual = a.pow(12).unwrap();
+
+        assert_eq!(actual, FieldElement::new(7, p).unwrap());
+    }
+
+    #[test]
+    fn div() {
+        let p = 19;
+        let a = FieldElement::new(2, p).unwrap();
+        let b = FieldElement::new(7, p).unwrap();
+
+        let actual = a.div(b).unwrap();
+
+        assert_eq!(actual, FieldElement::new(3, p).unwrap());
+
+        let a = FieldElement::new(7, p).unwrap();
+        let b = FieldElement::new(5, p).unwrap();
+
+        let actual = a.div(b).unwrap();
+
+        assert_eq!(actual, FieldElement::new(9, p).unwrap());
+    }
+}
---
+++ src/point.rs
@@ -0,0 +1,140 @@
+use std::ops::Mul;
+
+use crate::Error;
+
+#[derive(Copy, Clone, Debug)]
+pub enum Coordinate {
+    Real { x: i64, y: i64 },
+    Infinity,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Point {
+    pub a: i64,
+    pub b: i64,
+    pub coord: Coordinate,
+}
+
+#[allow(unused)]
+impl Point {
+    fn new(x: Option<i64>, y: Option<i64>, a: i64, b: i64) -> Result<Self, Error> {
+        match (x, y) {
+            (Some(x_val), Some(y_val)) => {
+                if y_val.pow(2) != x_val.pow(3) + std::ops::Mul::mul(a, x_val) + b {
+                    return Err(Error::ValueError);
+                }
+
+                Ok(Self {
+                    a,
+                    b,
+                    coord: Coordinate::Real { x: x_val, y: y_val },
+                })
+            }
+            (None, None) => Ok(Self {
+                a,
+                b,
+                coord: Coordinate::Infinity,
+            }),
+            _ => return Err(Error::ValueError),
+        }
+    }
+
+    fn add(&self, other: &Self) -> Result<Self, Error> {
+        if self.a != other.a || self.b != other.b {
+            return Err(Error::ValueError);
+        }
+
+        match (self.coord, other.coord) {
+            (
+                Coordinate::Real { x, y },
+                Coordinate::Real {
+                    x: other_x,
+                    y: other_y,
+                },
+            ) => {
+                // Additive inverse: same x, different y. Returns the point at infinity
+                if x == other_x && y != other_y {
+                    return Ok(Point {
+                        a: self.a,
+                        b: self.b,
+                        coord: Coordinate::Infinity,
+                    });
+                }
+
+                let slope = (other_y - y) / (other_x - x);
+                let new_x = slope.pow(2) - x - other_x;
+                let new_y = slope.mul(x - new_x) - y;
+
+                Ok(Point {
+                    a: self.a,
+                    b: self.b,
+                    coord: Coordinate::Real { x: new_x, y: new_y },
+                })
+            }
+            (Coordinate::Real { x, y }, Coordinate::Infinity) => Ok(*other),
+            (Coordinate::Infinity, Coordinate::Real { x, y }) => Ok(*self),
+            (Coordinate::Infinity, Coordinate::Infinity) => Ok(Point {
+                a: self.a,
+                b: self.b,
+                coord: Coordinate::Infinity,
+            }),
+        }
+    }
+}
+
+impl std::cmp::PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        if self.a != other.a || self.b != other.b {
+            return false;
+        }
+
+        match (self.coord, other.coord) {
+            (
+                Coordinate::Real { x, y },
+                Coordinate::Real {
+                    x: other_x,
+                    y: other_y,
+                },
+            ) => x == other_x && y == other_y,
+            (Coordinate::Infinity, Coordinate::Infinity) => true,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::point::Point;
+
+    #[test]
+    fn point() {
+        let a = 5;
+        let b = 7;
+
+        let p1 = Point::new(Some(2), Some(4), a, b);
+        assert!(p1.is_err());
+
+        let p2 = Point::new(Some(-1), Some(-1), a, b);
+        assert!(p2.is_ok());
+
+        let p3 = Point::new(Some(18), Some(77), a, b);
+        assert!(p3.is_ok());
+
+        let p4 = Point::new(Some(5), Some(7), a, b);
+        assert!(p4.is_err());
+    }
+
+    #[test]
+    fn add() {
+        let a = 5;
+        let b = 7;
+
+        let p1 = Point::new(Some(2), Some(5), a, b).unwrap();
+        let p2 = Point::new(Some(-1), Some(-1), a, b).unwrap();
+        let p3 = p1.add(&p2).unwrap();
+
+        let expected = Point::new(Some(3), Some(-7), a, b).unwrap();
+
+        assert_eq!(p3, expected);
+    }
+}
